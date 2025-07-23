223274f47f51d6b52e7a86464f59675fb464741c
Tushar Sain <skytusharkum786@gmail.com>
2025-07-23 12:27:06 +0530

    Update index.js

--- client/src/Store/admin/product-slice/index.js
+++ client/src/Store/admin/product-slice/index.js
@@ -10,7 +10,7 @@ export const addNewProduct = createAsyncThunk(
     '/products/addnewproduct',
     async (formData) => {
         const result = await axios.post(
-            'http://localhost:5000/api/admin/products/add',
+            'https://fashion-website-backend.vercel.app/api/admin/products/add',
             formData,
             {
                 headers: {
@@ -26,7 +26,7 @@ export const fetchAllProducts = createAsyncThunk(
     '/products/fetchAllProducts',
     async () => {
         const result = await axios.get(
-            'http://localhost:5000/api/admin/products/get'
+            'https://fashion-website-backend.vercel.app/api/admin/products/get'
         );
         return result?.data;
     }
@@ -36,7 +36,7 @@ export const editProduct = createAsyncThunk(
     '/products/editProduct',
     async ({ id, formData }) => {
         const result = await axios.put(
-            `http://localhost:5000/api/admin/products/edit/${id}`,
+            `https://fashion-website-backend.vercel.app/api/admin/products/edit/${id}`,
             formData,
             {
                 headers: {
@@ -52,7 +52,7 @@ export const deleteProduct = createAsyncThunk(
     '/products/deleteProduct',
     async (id) => {
         const result = await axios.delete(
-            `http://localhost:5000/api/admin/products/delete/${id}`
+            `https://fashion-website-backend.vercel.app/api/admin/products/delete/${id}`
         );
         return result?.data;
     }
@@ -68,7 +68,6 @@ const adminProductsSlice = createSlice({
                 state.isLoading = true;
             })
             .addCase(fetchAllProducts.fulfilled, (state, action) => {
-                
                 state.isLoading = false;
                 state.productList = action.payload.data;
             })
