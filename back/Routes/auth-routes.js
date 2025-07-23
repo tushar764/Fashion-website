738157d079131ffd76ef18e6b78f310811cfb29a
Tushar Sain <skytusharkum786@gmail.com>
2025-07-24 00:44:26 +0530

    Update auth-routes.js

--- back/Routes/auth-routes.js
+++ back/Routes/auth-routes.js
@@ -1,29 +1,31 @@
-const express = require('express')
-const {registerUser,loginUser,logoutUser,authMiddleware} =require('../Controllers/auth/auth-controller')
+const express = require('express');
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  authMiddleware,
+} = require('../Controllers/auth/auth-controller');
+
 const router = express.Router();
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 
-
-router.get("/check-auth", authMiddleware, (req, res) => {
-    const user = req.user;
-    if (!user) {
-        return res.status(401).json({
-            success: false,
-            message: "Unauthorized user!",
-        });
-    }
-
-    res.status(200).json({
-        success: true,
-        message: "Authenticated user!",
-        user,
+router.get('/check-auth', authMiddleware, (req, res) => {
+  const user = req.user;
+  if (!user) {
+    return res.status(401).json({
+      success: false,
+      message: 'Unauthorized user!',
     });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: 'Authenticated user!',
+    user,
+  });
 });
 
-
-
-module.exports=router;
-
-
+module.exports = router;
